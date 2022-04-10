0a508cf9700c2a689782143c3b7f2f3a43faedc9
rinat229 <rdkhaliulin@miem.hse.ru>
2022-04-10 19:54:28 +0300

    changed logic

--- datas.py
+++ datas.py
@@ -4,8 +4,16 @@ from dataclasses import dataclass
 from datetime import datetime
 
 import requests
-from .info import HEADERS, URL
+from info import HEADERS, URL
 
+
+region = {}
+def get_region_info(id_for_query):
+    global region
+    if id_for_query not in region:
+        returned_region_info = Base().request("machine_regions", "", "", "")
+        region = {responses["id"]: responses["name"] for responses in returned_region_info}
+    return region[id_for_query]
 
 @dataclass
 class ExcelInfo:
@@ -23,21 +31,16 @@ class ExcelInfo:
 class Base:
     """Base class for init"""
 
-    def __init__(self, id_for_query: str):
-        """init"""
-        self.get_task_info(id_for_query)
-
-    def get_task_info(self, id_for_query: str):
-        """base"""
-
+    def request(self, query_type: str, query_value: str, query_key: str, not_empty_query: str="="):
+        defined_url = f"{URL}//{query_type}//?{query_value}{not_empty_query}{query_key}"
+        return requests.get(defined_url, headers=HEADERS).json()["data"]
 
 class Task(Base):
     """class for get requests of task info"""
 
-    def get_task_info(self, id_for_query: str):
+    def __init__(self, id_for_query: str):
         """get info by request"""
-        defined_url = URL + "machine_tasks/?id=" + str(id_for_query)
-        returned_info = requests.get(defined_url, headers=HEADERS).json()["data"][0]
+        returned_info = self.request("machine_tasks", "id", id_for_query)[0]
 
         self.start_time = returned_info["start_time"]
         self.end_time = returned_info["end_time"]
@@ -58,44 +61,36 @@ class Task(Base):
 class Machine(Base):
     """class for get requests of machine info"""
 
-    def get_task_info(self, id_for_query: str):
+    def __init__(self, id_for_query: str):
         """get info by request"""
-        defined_url = URL + "machines/?id=" + str(id_for_query)
-        returned_info = requests.get(defined_url, headers=HEADERS).json()["data"][0]
+        returned_info = self.request("machines", "id", id_for_query)[0]
         self.name = returned_info["name"]
-
-        defined_url = URL + "machine_region_mapping_items/?machine_id=" + str(id_for_query)
-        returned_info = requests.get(defined_url, headers=HEADERS).json()["data"][0]
-        defined_url = URL + "machine_regions/?id=" + str(returned_info["machine_region_id"])
-        returned_info = requests.get(defined_url, headers=HEADERS).json()["data"][1]
-        self.region = returned_info["name"]
+        returned_machines_info = self.request("machine_region_mapping_items","machine_id" , id_for_query)[0]
+        self.region = get_region_info(returned_machines_info["machine_region_id"])
 
 
 class Driver(Base):
     """class for get requests of driver info"""
 
-    def get_task_info(self, id_for_query: str):
+    def __init__(self, id_for_query: str):
         """get info by request"""
-        defined_url = URL + "users/?id=" + str(id_for_query)
-        returned_info = requests.get(defined_url, headers=HEADERS).json()["data"][0]
+        returned_info = self.request("users", "id", id_for_query)[0]
         self.driver_name = returned_info["username"]
 
 
 class WorkType(Base):
     """class for get requests of work_type info"""
 
-    def get_task_info(self, id_for_query: str):
+    def __init__(self, id_for_query: str):
         """get info by request"""
-        defined_url = URL + "work_types/?id=" + str(id_for_query)
-        returned_info = requests.get(defined_url, headers=HEADERS).json()["data"][0]
+        returned_info = self.request("work_types", "id", id_for_query)[0]
         self.work_type_name = returned_info["name"]
 
 
 class Implement(Base):
     """class for get requests of implement info"""
 
-    def get_task_info(self, id_for_query: str):
+    def __init__(self, id_for_query: str):
         """get info by request"""
-        defined_url = URL + "implements/?id=" + str(id_for_query)
-        returned_info = requests.get(defined_url, headers=HEADERS).json()["data"][0]
+        returned_info = self.request("implements", "id", id_for_query)[0]
         self.implement_name = returned_info["name"]
