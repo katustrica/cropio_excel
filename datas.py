2117c7c18901281c8e024dbfd9d16f6e6158f7e6
rinat229 <rdkhaliulin@miem.hse.ru>
2022-04-10 18:32:31 +0300

    added typing

--- datas.py
+++ datas.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 from datetime import datetime
 
 import requests
-from info import HEADERS, URL
+from .info import HEADERS, URL
 
 
 @dataclass
@@ -23,18 +23,18 @@ class ExcelInfo:
 class Base:
     """Base class for init"""
 
-    def __init__(self, id_for_query):
+    def __init__(self, id_for_query: str):
         """init"""
         self.get_task_info(id_for_query)
 
-    def get_task_info(self, id_for_query):
+    def get_task_info(self, id_for_query: str):
         """base"""
 
 
 class Task(Base):
     """class for get requests of task info"""
 
-    def get_task_info(self, id_for_query):
+    def get_task_info(self, id_for_query: str):
         """get info by request"""
         defined_url = URL + "machine_tasks/?id=" + str(id_for_query)
         returned_info = requests.get(defined_url, headers=HEADERS).json()["data"][0]
@@ -58,7 +58,7 @@ class Task(Base):
 class Machine(Base):
     """class for get requests of machine info"""
 
-    def get_task_info(self, id_for_query):
+    def get_task_info(self, id_for_query: str):
         """get info by request"""
         defined_url = URL + "machines/?id=" + str(id_for_query)
         returned_info = requests.get(defined_url, headers=HEADERS).json()["data"][0]
@@ -74,7 +74,7 @@ class Machine(Base):
 class Driver(Base):
     """class for get requests of driver info"""
 
-    def get_task_info(self, id_for_query):
+    def get_task_info(self, id_for_query: str):
         """get info by request"""
         defined_url = URL + "users/?id=" + str(id_for_query)
         returned_info = requests.get(defined_url, headers=HEADERS).json()["data"][0]
@@ -84,7 +84,7 @@ class Driver(Base):
 class WorkType(Base):
     """class for get requests of work_type info"""
 
-    def get_task_info(self, id_for_query):
+    def get_task_info(self, id_for_query: str):
         """get info by request"""
         defined_url = URL + "work_types/?id=" + str(id_for_query)
         returned_info = requests.get(defined_url, headers=HEADERS).json()["data"][0]
@@ -94,7 +94,7 @@ class WorkType(Base):
 class Implement(Base):
     """class for get requests of implement info"""
 
-    def get_task_info(self, id_for_query):
+    def get_task_info(self, id_for_query: str):
         """get info by request"""
         defined_url = URL + "implements/?id=" + str(id_for_query)
         returned_info = requests.get(defined_url, headers=HEADERS).json()["data"][0]
